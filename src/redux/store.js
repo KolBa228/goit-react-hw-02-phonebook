885bd474340162e484fcdeedf86d818a61744d66
KolBa228 <technotrashinfo@gmail.com>
2023-09-03 18:48:49 +0100

    done persits

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,27 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { initialState } from './state';
 import { rootReducer } from './slice';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistContactsReducer = persistReducer(
+  persistConfig,
+  rootReducer
+)
 
 export const store = configureStore({
   reducer: {
-    contacts: rootReducer,
+    contacts: persistContactsReducer,
   },
 });
+
+export const persistor = persistStore(store)
+
 
 export const selectContacts = state => state.contacts.contacts;
 export const selectFilter = state => state.contacts.filter;
